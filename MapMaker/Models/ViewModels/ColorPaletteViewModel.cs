28f1a57ad6c2ecc741f621b2356427b137fa91ae
= <balfour.williamc5@gmail.com>
2023-11-24 15:34:03 -0500

    Add custom color palettes

--- MapMaker/Models/ViewModels/ColorPaletteViewModel.cs
+++ MapMaker/Models/ViewModels/ColorPaletteViewModel.cs
@@ -6,8 +6,13 @@
         private string _landColor = "#4a6741";
         private string _textColor = "#000000";
 
-        public ColorPaletteViewModel() => new ColorPaletteViewModel(_canvasColor, _landColor, _textColor);
-        
+        public ColorPaletteViewModel()
+        {
+            CanvasColor = _canvasColor; 
+            LandColor = _landColor;
+            TextColor = _textColor;
+        }
+
         public ColorPaletteViewModel(string canvasColor, string landColor, string textColor)
         {
             CanvasColor = canvasColor;
